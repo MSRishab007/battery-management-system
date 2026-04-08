004946d53e71eaff9fe6e1cb75cb212682a8b9f8
MSRishab007 <s.rishab0807@gmail.com>
2026-04-08 18:00:32 +0530

    Fix the USB bug

--- src/HAL/Relays.cpp
+++ src/HAL/Relays.cpp
@@ -9,7 +9,7 @@ constexpr int PIN_RELAY_RECOVERY  = 4;
 constexpr int PIN_BALANCE_CELL_1  = 16;
 constexpr int PIN_BALANCE_CELL_2  = 17;
 constexpr int PIN_BALANCE_CELL_3  = 18;
-constexpr int PIN_BALANCE_CELL_4  = 19;
+constexpr int PIN_BALANCE_CELL_4  = 19; 
 
 const int BALANCE_PINS[NUM_CELLS] = {
     PIN_BALANCE_CELL_1, PIN_BALANCE_CELL_2, 
@@ -27,6 +27,12 @@ bool Relays::mockBalancePins[NUM_CELLS] = {false};
 
 void Relays::init() {
 #ifdef ARDUINO
+    Serial.println("   -> [RELAYS] HARDWARE AIR-GAPPED. Pins protected.");
+    
+    // =========================================================
+    // PHYSICAL PINS DISABLED TO PREVENT USB SHORT-CIRCUIT
+    // =========================================================
+    /*
     pinMode(PIN_RELAY_CHARGE, OUTPUT);
     pinMode(PIN_RELAY_DISCHARGE, OUTPUT);
     pinMode(PIN_RELAY_RECOVERY, OUTPUT);
@@ -39,6 +45,7 @@ void Relays::init() {
     for(int i = 0; i < NUM_CELLS; i++) {
         digitalWrite(BALANCE_PINS[i], LOW);
     }
+    */
 #else
     mockChargePin = false;
     mockDischargePin = false;
@@ -49,6 +56,10 @@ void Relays::init() {
 
 void Relays::update(const BmsRecord& record) {
 #ifdef ARDUINO
+    // HARDWARE COMMANDS COMMENTED OUT. 
+    // We only read the variables into the void to keep the compiler happy.
+    
+    /*
     digitalWrite(PIN_RELAY_CHARGE, record.cmdChargeRelay ? HIGH : LOW);
     digitalWrite(PIN_RELAY_DISCHARGE, record.cmdDischargeRelay ? HIGH : LOW);
     digitalWrite(PIN_RELAY_RECOVERY, record.cmdRecoveryRelay ? HIGH : LOW);
@@ -56,6 +67,7 @@ void Relays::update(const BmsRecord& record) {
     for(int i = 0; i < NUM_CELLS; i++) {
         digitalWrite(BALANCE_PINS[i], record.balanceEnables[i] ? HIGH : LOW);
     }
+    */
 #else
     mockChargePin = record.cmdChargeRelay;
     mockDischargePin = record.cmdDischargeRelay;
--- src/HAL/Relays.h
+++ src/HAL/Relays.h
@@ -1,17 +1,10 @@
 #pragma once
+
 #include "../../include/DataTypes.h"
+#include <Arduino.h>
 
 class Relays {
 public:
     static void init();
-    
     static void update(const BmsRecord& record);
-
-
-#ifndef ARDUINO
-    static bool mockChargePin;
-    static bool mockDischargePin;
-    static bool mockRecoveryPin;
-    static bool mockBalancePins[NUM_CELLS];
-#endif
 };
